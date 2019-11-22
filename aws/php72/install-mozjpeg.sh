1356b799bc9e8da13b77997333053edb1477d970
tom <t.vitureau@purjus.fr>
2019-11-22 13:15:28 +0100

    clean install mozjpeg

--- aws/php72/install-mozjpeg.sh
+++ aws/php72/install-mozjpeg.sh
@@ -3,22 +3,23 @@ set -eu
 
 VERSION=3.3.1
 
+cd /tmp
+
 apt-get update
-apt-get install --yes --no-install-recommends autoconf automake libtool nasm make pkg-config
+apt-get install --yes --no-install-recommends automake libtool nasm # autoconf make
 rm -rf /var/lib/apt/lists/*
 
-cd $(mktemp -d)
+wget https://github.com/mozilla/mozjpeg/archive/v${VERSION}.tar.gz
+tar -xvf v${VERSION}.tar.gz
 
-wget "https://github.com/mozilla/mozjpeg/archive/v$VERSION.tar.gz"
-tar -xvf "v$VERSION.tar.gz"
-
-cd "mozjpeg-$VERSION"
+cd mozjpeg-${VERSION}/
 
 autoreconf -fiv
 ./configure
-make
 make install
 
-apt-get purge --yes --auto-remove libtool nasm
+ln -s /opt/libmozjpeg/bin/jpegtran /usr/local/bin/mozjpeg
 
-ln -s /opt/mozjpeg/bin/jpegtran /usr/bin/mozjpeg
+cd ..
+rm -Rf v${VERSION}.tar.gz mozjpeg-${VERSION}
+apt-get remove -qq automake autotools-dev libtool nasm
